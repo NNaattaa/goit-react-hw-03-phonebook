e72c24932fa964a26387e9212f1556e27e6ab9b3
NNaattaa <50961364+NNaattaa@users.noreply.github.com>
2020-07-26 00:58:40 +0300

    Updates

---
+++ precache-manifest.55df25d6c4e9823dc901b11a1c8fb677.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "048226dfcae5b0de711bd9ee28d980d6",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "20a770741070a6ac1911",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.2526452b.chunk.css"
+  },
+  {
+    "revision": "60662148c60d6944de83",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.63717b8e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.63717b8e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "20a770741070a6ac1911",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.660b28ec.chunk.js"
+  },
+  {
+    "revision": "8b583280b34de9d01efa",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.8935b325.js"
+  }
+]);
